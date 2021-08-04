a38c287183ac7d58c4793f538443d63a8d488c9a
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-08-04 15:01:23 +0700

    Multithreaded scraping

--- src/scrape_historical_tableau_data.py
+++ src/scrape_historical_tableau_data.py
@@ -3,9 +3,16 @@ import os
 from tableauscraper import TableauScraper as TS
 from util import json_load
 from scrape_tablueau_data import scrape_tablueau
+from multiprocessing.pool import ThreadPool
+
+
+def scrape_wrapper(parameters: tuple):
+    print("Processing:", parameters[1])
+    ts = TS()
+    scrape_tablueau(ts, url=parameters[0], out_path=out_path, date=parameters[1])
+
 
 if __name__ == "__main__":
-    ts = TS()
 
     url_dict = {}
     for i in glob.glob("../dashboard_links/covid-dashboard-link-2021-*.json"):
@@ -13,8 +20,12 @@ if __name__ == "__main__":
 
     out_path = "../wiki/tableau_dump"
     os.makedirs(out_path, exist_ok=True)
+    scrape_parameters = []
     for date, url in url_dict.items():
         date = date.split("/")
         date = f"{date[2]}-{date[0]}-{date[1]}"
-        print("Processing:", date)
-        scrape_tablueau(ts, url, out_path, date)
+        scrape_parameters.append((url, date))
+
+    with ThreadPool(16) as p:
+        p.map(scrape_wrapper, scrape_parameters)
+
