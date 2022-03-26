1f106bb02c4c1560673c970277ef7c664828539a
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-03-26 11:28:24 +0700

    mannual update: 2022-03-25

--- src/scrape_historical_tableau_data.py
+++ src/scrape_historical_tableau_data.py
@@ -15,7 +15,7 @@ def scrape_wrapper(parameters: tuple):
 if __name__ == "__main__":
 
     url_dict = {}
-    for i in glob.glob("../dashboard_links/covid-dashboard-link-2021-12-29-2022-01-03.json"):
+    for i in glob.glob("../dashboard_links/covid-dashboard-link-2022-03-25-2022-03-25.json"):
         url_dict.update(json_load(i))
 
     out_path = "../wiki/tableau_dump"
